59edd6a150bb74230847b8e9860e2caa5785215b
Logan Sage <lvdsage@gmail.com>
2021-03-13 12:32:05 -0700

    changing file structure to fix herroku error

--- employee-directory/src/components/Container/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Container() {
-  return <div className = "wrapper"></div>
-           
-}
-
-export default Container;
--- employee-directory/src/components/Header/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Header() {
-  return <h1>Employee Directory</h1>
-           
-}
-
-export default Header;
