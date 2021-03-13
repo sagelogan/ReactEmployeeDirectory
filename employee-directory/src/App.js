7f4fe02e54b315ad5f622fea59796346250cd5eb
Logan Sage <lvdsage@gmail.com>
2021-03-12 20:42:59 -0700

    linkage of components and 'useeffect'/state

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect,} from "react";
+import Header from "./components/Header";
+import Searchbar from "./components/Searchbar";
+import List from "./components/List";
+import ListContext from "./components/utils/ListContext";
+import people from "./directory.json";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [pageState, setPageState] = useState({
+    directory : people,
+    search:"",
+  })
+
+  useEffect(()=> {
+    if(!pageState.search){
+      setPageState({...pageState, directory: people})
+    }else{
+      setPageState({...pageState, directory: people.filter(user=> (`${user.name.first} ${user.name.last}`).toLowerCase().includes(pageState.search) === true)})
+    }
+  },[pageState])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <ListContext.Provider value={pageState}>
+              <Header />
+              <Searchbar/>
+              <List/>
+              </ListContext.Provider>
   );
 }
 
